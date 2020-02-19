54f0757f4bfda84e04f519393b722b84ac3aae3e
guikai <916126728@qq.com>
2020-02-19 15:59:26 +0800

    个推框架集成PushId获取

--- common/src/main/java/net/guikai/italker/common/app/BaseApplication.java
+++ common/src/main/java/net/guikai/italker/common/app/BaseApplication.java
@@ -1,11 +1,16 @@
 package net.guikai.italker.common.app;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
 
 import net.qiujuer.genius.kit.handler.Run;
 import net.qiujuer.genius.kit.handler.runable.Action;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description: 自定义定制Application
@@ -13,16 +18,59 @@ import net.qiujuer.genius.kit.handler.runable.Action;
  */
 public class BaseApplication extends Application {
 
-    private static Application instance;
-
-    public static Application getInstance() {
-        return instance;
-    }
+    private static BaseApplication instance;
+    private List<Activity> activities = new ArrayList<>();
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activities.add(activity);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                activities.remove(activity);
+            }
+        });
+    }
+
+    /**
+     * 外部获取单例
+     *
+     * @return Application
+     */
+    public static BaseApplication getInstance() {
+        return instance;
     }
 
     /**
