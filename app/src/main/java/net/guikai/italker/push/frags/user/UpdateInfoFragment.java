d06728d5384193e9bd4ecc839ca98a99826fd54d
guikai <916126728@qq.com>
2020-02-21 20:22:17 +0800

    图片剪切UCrop库集成

--- app/src/main/java/net/guikai/italker/push/frags/user/UpdateInfoFragment.java
+++ app/src/main/java/net/guikai/italker/push/frags/user/UpdateInfoFragment.java
@@ -1,9 +1,17 @@
 package net.guikai.italker.push.frags.user;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.yalantis.ucrop.UCrop;
+
+import net.guikai.italker.common.app.BaseApplication;
 import net.guikai.italker.common.app.PresenterFragment;
 import net.guikai.italker.common.widget.PortraitView;
 import net.guikai.italker.factory.presenter.user.UpdateInfoContract;
@@ -12,8 +20,12 @@ import net.guikai.italker.push.R;
 import net.guikai.italker.push.frags.media.GalleryFragment;
 import net.qiujuer.genius.ui.widget.Loading;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Description: 用户更新信息的界面
@@ -37,6 +49,10 @@ public class UpdateInfoFragment extends PresenterFragment<UpdateInfoContract.Pre
     @BindView(R.id.btn_submit)
     Button mSubmit;
 
+    // 头像的本地路径
+    private String mPortraitPath;
+    private boolean isMan = true;
+
     @Override
     protected UpdateInfoContract.Presenter initPresenter() {
         return new UpdateInfoPresenter(this);
@@ -53,10 +69,67 @@ public class UpdateInfoFragment extends PresenterFragment<UpdateInfoContract.Pre
                 .setListener(new GalleryFragment.OnSelectedListener() {
                     @Override
                     public void onSelectedImage(String path) {
+                        UCrop.Options options = new UCrop.Options();
+                        // 设置图片处理的格式JPEG
+                        options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+                        // 设置压缩后的图片精度
+                        options.setCompressionQuality(96);
 
+                        // 得到头像的缓存地址
+                        File dPath = BaseApplication.getPortraitTmpFile();
+
+                        // 发起剪切
+                        UCrop.of(Uri.fromFile(new File(path)), Uri.fromFile(dPath))
+                                .withAspectRatio(1, 1) // 1比1比例
+                                .withMaxResultSize(520, 520) // 返回最大的尺寸
+                                .withOptions(options) // 相关参数
+                                .start(getActivity());
                     }
                 })
                 .show(getChildFragmentManager(),GalleryFragment.class.getName());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 收到从Activity传递过来的回调，然后取出其中的值进行图片加载
+        // 如果是我能够处理的类型
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            // 通过UCrop得到对应的Uri
+            final Uri resultUri = UCrop.getOutput(data);
+            if (resultUri != null) {
+                loadPortrait(resultUri);
+            }
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            BaseApplication.showToast(R.string.data_rsp_error_unknown);
+        }
+    }
+
+    /**
+     * 加载Uri到当前的头像中
+     *
+     * @param uri Uri
+     */
+    private void loadPortrait(Uri uri) {
+        // 得到头像地址
+        mPortraitPath = uri.getPath();
+
+        Glide.with(this)
+                .load(uri)
+                .asBitmap()
+                .centerCrop()
+                .into(mPortrait);
+    }
+
+    @OnClick(R.id.im_sex)
+    void onSexClick() {
+        // 性别图片点击的时候触发
+        isMan = !isMan; // 反向性别
+
+        Drawable drawable = getResources().getDrawable(isMan ?
+                R.drawable.ic_sex_man : R.drawable.ic_sex_woman);
+        mSex.setImageDrawable(drawable);
+        // 设置背景的层级，切换颜色
+        mSex.getBackground().setLevel(isMan ? 0 : 1);
     }
 
     @Override
--- common/src/main/java/net/guikai/italker/common/app/BaseApplication.java
+++ common/src/main/java/net/guikai/italker/common/app/BaseApplication.java
@@ -2,13 +2,16 @@ package net.guikai.italker.common.app;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
 
 import net.qiujuer.genius.kit.handler.Run;
 import net.qiujuer.genius.kit.handler.runable.Action;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,6 +67,19 @@ public class BaseApplication extends Application {
         });
     }
 
+    // 退出所有
+    public void finishAll(){
+        for (Activity activity : activities) {
+            activity.finish();
+        }
+
+        showAccountView(this);
+    }
+
+    protected void showAccountView(Context context){
+
+    }
+
     /**
      * 外部获取单例
      *
@@ -71,6 +87,39 @@ public class BaseApplication extends Application {
      */
     public static BaseApplication getInstance() {
         return instance;
+    }
+
+    /**
+     * 获取缓存文件夹地址
+     *
+     * @return 当前APP的缓存文件夹地址
+     */
+    public static File getCacheDirFile() {
+        return instance.getCacheDir();
+    }
+
+    /**
+     * 获取头像的临时存储文件地址
+     *
+     * @return 临时文件
+     */
+    public static File getPortraitTmpFile() {
+        // 得到头像目录的缓存地址
+        File dir = new File(getCacheDirFile(), "portrait");
+        // 创建所有的对应的文件夹
+        //noinspection ResultOfMethodCallIgnored
+        dir.mkdirs();
+        // 删除旧的一些缓存为文件
+        File[] files = dir.listFiles();
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                //noinspection ResultOfMethodCallIgnored
+                file.delete();
+            }
+        }
+        // 返回一个当前时间戳的目录文件地址
+        File path = new File(dir, SystemClock.uptimeMillis() + ".jpg");
+        return path.getAbsoluteFile();
     }
 
     /**
